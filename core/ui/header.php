15c9394d9f85d3bd74a3ccea6aef57e092e3e6a1
zlysie <realoikmo@gmail.com>
2025-08-10 02:44:15 +0100

    Template finished
    Header finished and headers added
    Working on frontpage, halfway done with the it

--- core/ui/header.php
+++ core/ui/header.php
@@ -2,84 +2,45 @@
 	require_once $_SERVER['DOCUMENT_ROOT'].'/core/utilities/userutils.php';
 	$header_check_user = UserUtils::RetrieveUser();
 ?>
-<?php if($header_check_user == null): ?>
-<style>
-	#Body, #Footer { margin-left: unset !important; }
-</style>
-<?php endif ?>
 <div id="Header">
-	<div id="Topbar">
-		<a href="/" id="Logo">
-			<img src="/images/spinner100x100.gif" data-src="/images/menus/IOTA128x.png" height="36" style="margin-left:10px;">
-		</a>
-		<?php if($header_check_user != null): ?>
-		<div id="Links">
-			<a href="/games">Games</a>
-			<span class="Separator">&nbsp;|&nbsp;</span>
-			<a href="/catalog">Catalog</a>
-			<span class="Separator">&nbsp;|&nbsp;</span>
-			<a href="/people">People</a>
-		</div>
-		<div id="Search">
-			<input type="text" placeholder="Search...">
-			<div id="Queries" style="display:none">
-				<div id="Query">
-					<a href="/games" typa="Games">Search "" in Games</a>
-				</div>
-				<div id="Query">
-					<a href="/catalog" typa="Catalog">Search "" in Catalog</a>
-				</div>
-				<div id="Query">
-					<a href="/people" typa="People">Search "" in People</a>
-				</div>
-			</div>
-		</div>
-		<div id="Controls">
-			<a>
-				<img src="/images/spinner16x16.gif" data-src="/images/icons/bling.png" height="32">
-				<span>100000</span>
-			</a>
-			<a href="/my/messages">
-				<img src="/images/spinner16x16.gif" data-src="">
-			</a>
-			<a id="SettingsButton">
-				<img src="/images/spinner16x16.gif" data-src="/images/icons/settings.png">
-			</a>
-			<div id="SettingsPanel" style="display: none">
-				<div id="Option"><a href="">Settings</a></div>
-				<div id="Option"><a href="javascript:Iota.Logout()">Log out</a></div>
-			</div>
-		</div>
-		<?php else: ?>
-		<div id="Links">
-			<a href="/register">Register</a>
-			<span class="Separator">&nbsp;|&nbsp;</span>
-			<a href="/">Login</a>
-		</div>	
-		<?php endif ?>
-	</div>
 	<?php if($header_check_user != null): ?>
-	<div id="Sidebar">
-		<div style="margin-top:5px; width:100%;"></div>
-		
-		<a href="/home" id="Navigation">
-			<img src="/images/spinner16x16.gif" data-src="/images/icons/home_coloured.png">Home
-		</a>
-		<a href="/my/profile" id="Navigation">
-			<img src="/images/spinner16x16.gif" data-src="/images/icons/profile_coloured.png">Profile
-		</a>
-		<a href="/my/friends" id="Navigation">
-			<img src="/images/spinner16x16.gif" data-src="/images/icons/friends_coloured.png">Friends
-		</a>
-		<a href="/my/character" id="Navigation">
-			<img src="/images/spinner16x16.gif" data-src="/images/icons/avatar_coloured.png">Avatar
-		</a>
-		<a href="/my/stuff" id="Navigation">
-			<img src="/images/spinner16x16.gif" data-src="/images/icons/inventory_coloured.png">Inventory
-		</a>
-		<a href="/my/trades" id="Navigation">
-			<img src="/images/spinner16x16.gif" data-src="/images/icons/trading_coloured.png">Trades
-		</a>
+	<div id="ProfileSign" logged="true">
+		<img id="background" src="/images/header/signs/profile.png"> <!-- DO NOT FUCKING REMOVE -->
+		<div id="UsernameRow">
+			YOU ARE: <br>
+			<a href="/users/<?= $header_check_user->id ?>/profile"><?= $header_check_user->name ?></a>
+		</div>
+		<hr>
+		<div id="CreditsRow">
+			<span title="Traffic Cones (ROBUX)"><img src="/images/icons/traffic_cone.png"> 1</span> <span class="Separator">|</span>
+			<span title="Traffic Lights (TIX)"><img src="/images/icons/traffic_light.png"> 1</span>
+
+			<hr>
+			
+			<span title="Your messages"><a href="/my/messages"><img src="/images/icons/messages.png"> 1</a></span> <span class="Separator">|</span>
+			<span title="Your friends"><a href="/my/friends"><img src="/images/icons/friends.png"> 1</a></span>
+		</div>
+	</div>
+	<a id="LogoutSign" href="/">LOGOUT</a>
+	<?php else: ?>
+	<div id="ProfileSign" logged="true">
+		<img id="background" src="/images/header/signs/profile.png"> <!-- DO NOT FUCKING REMOVE -->
+		<a href="" id="RegisterSign">Register</a>
+		<img src="/images/sign_2way.png" style="width: 72px;padding: 10px 0;padding-top: 30px;padding-bottom:5px;z-index: 2;position: relative;">
+		<a href="" id="LoginSign">Login</a>
 	</div>
 	<?php endif ?>
+	<div id="Logo">
+		<a href="/">
+			<img src="/images/header/logo.png">
+		</a>
+	</div>
+	
+	<div id="Links" >
+		<?php if($header_check_user != null): ?><a href="/users/<?= $header_check_user->id ?>/profile">Profile</a><?php endif ?>
+		<a href="/games">Games</a>
+		<a href="/catalog">Catalog</a>
+		<a href="/people">People</a>
+		<a href="/forums">Forums</a>
+	</div>
 </div>
--- index.php
+++ index.php
@@ -1,106 +1,120 @@
-<?php
-	session_start();
-
-	require_once $_SERVER['DOCUMENT_ROOT'].'/core/utilities/userutils.php';
-	$user = UserUtils::RetrieveUser();
-
-	if($user != null) {
-		die(header("Location: /home"));
-	}
-
-	if(isset($_POST['Iota$Login$Username']) &&
-	   isset($_POST['Iota$Login$Password']) &&
-	   isset($_POST['Iota$Login$Submit'])) {
-		
-		$username = trim($_POST['Iota$Login$Username']);
-		$password = trim($_POST['Iota$Login$Password']);
-
-		$result = UserUtils::LoginUser($username, $password);
-
-		if($result["login"] != "Incorrect details provided!") {
-			die(header("Location: /home"));
-		} else {
-			$_SESSION['login_errors'] = $result;
-			die(header("Location: /"));
-		}
-	}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>Iota</title>
+		<title>Welcome to ANORRL!</title>
 		<link rel="icon" type="image/x-icon" href="/favicon.ico">
 		<link rel="stylesheet" href="/css/AllCSS.css">
 		<script src="/js/jquery.js"></script>
-		<script src="/js/loginchecker.js"></script>
-		<script src="/js/jquery.imageloader.js"></script>
-		<script>
-			$(function() {
-				$('img').imageloader();
-			});
-		</script>
 		<style>
-			#Body {
-				position:relative;
+			#IntroductionBox {
+				width: 500px;
+			}
+
+			#IntroductionBox h2 {
+				margin-bottom: 8px;
+			}
+
+			#IntroductionBox h3 {
+				margin-bottom: 0px;
+				margin-top: 0px;
+				margin-left: 10px;
+				font-size: 16px;
+			}
+
+			#IntroductionBox p {
+				margin-left: 15px;
+				margin-top: 5px;
+				margin-bottom: 10px;
+			}
+
+			#ShowcaseContainer {
+				margin-top: -195px;
+				float: right;
+			}
+
+			#ShowcaseContainer h3 {
+				margin: 0;
+				
+			}
+
+			h2, h3 {
+				font-style:italic;
+			}
+
+			#ShowcaseBox {
+				border: 3px solid black;
+				background: #282828;
+				width: 355px;
+				padding: 10px;
+			}
+
+			#ShowcaseBox #Showcase {
+				width: 327px;
+				height: 184px;
+				background: #1a1919;
+				border: 2px solid #090909;
+				margin: 0 auto; 
+				display:block;
+			}
+
+			#ShowcaseBox #Carousel {
+				border: 2px solid #090909;
+				border-top: none;
+				margin: 0 auto;
+				width:fit-content;
+				height:42px;
+
+				background: #171717;
+				
+			}
+
+			#ShowcaseBox #Carousel img {
+				display:inline;
+				width:42px;
+				height:42px;
+			}
+
+			#ShowcaseBox #Carousel img:hover,
+			#ShowcaseBox #Carousel img.selected {
+				background: rgba(255, 255, 255, 0.2);
 			}
 		</style>
 	</head>
 	<body>
 		<div id="Container">
-			<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/header.php'; ?>
-			<div id="WrapperBody" style="position:relative">
-				<div id="Body">
-					<pre>
-<?php print_r($_SESSION) ?>
-					</pre>
-					<div id="FormPanel">
-						
-						<form method="POST">
-							<p>
-								<h2>Welcome back!</h2>
-								<span>If you do not have an account then,<br><a href="/register">register here!</a></span>
-								<span class="Validator">
-									<?php 
-										if(isset($_SESSION['login_errors'])) {
-											echo $_SESSION['login_errors']['login'];
-										}
-									?>
-								</span>
-							</p>
-							<p>
-								<span>Username</span>
-								<span class="Validator" id="v_username">
-									<?php 
-										if(isset($_SESSION['login_errors'])) {
-											echo $_SESSION['login_errors']['username'];
-										}
-									?>
-								</span>
-								<input type="text" id="Iota_Login_Username" name="Iota$Login$Username" minlength="3" maxlength="20" required>
-							</p>
-							<p>
-								<span>Password</span>
-								<span class="Validator" id="v_password">
-									<?php 
-										if(isset($_SESSION['login_errors'])) {
-											echo $_SESSION['login_errors']['password'];
-										}
-									?>
-								</span>
-								<input type="password" id="Iota_Login_Password" name="Iota$Login$Password" minlength="7" required>
-							</p>
-							<p>
-								<input type="submit" id="Iota_Login_Submit" name="Iota$Login$Submit" value="Login">
-							</p>
-						</form>						
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/header.php'; ?>
+			<div id="Body">
+				<div id="BodyContainer">
+					<div id="IntroductionBox">
+						<h2>What is ANORRL?</h2>
+						<p><b>ANORRL</b> is a 2012 "Old Roblox Revival" which aims to bring a fresh new look at the past!</p>
+						<p><b>ANORRL</b> stands for <code>"<b>AN</b>other <b>O</b>ld <b>R</b>oblox <b>R</b>evival <b>L</b>ol"</code></p>
+						<h3>So what is a "Old Roblox Revival"</h3>
+						<p>
+							It is a project that aims to recreate and bring life into relatively old clients that ROBLOX has created in the past.<br>
+							<b>Please note that</b>: These revivals do tend to be private (along with this one) as to ensure the maximum security because not only are These
+							clients old, they are also insecure!
+						</p>
 					</div>
+					<div id="ShowcaseContainer">
+						<h3>Showcase</h3>
+						<div id="ShowcaseBox">
+						<img id="Showcase" src="/images/avatar.png">
+							<div id="Carousel">
+								<img class="selected" src="/images/avatar.png">
+								<img src="/images/avatar.png">
+								<img src="/images/avatar.png">
+								<img src="/images/avatar.png">
+								<img src="/images/avatar.png">
+							</div>
+						</div>
+						
+					</div>
+					<br style="clear:both">
+					
 				</div>
 				<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/footer.php'; ?>
 			</div>
 		</div>
 	</body>
 </html>
-<?php 
-	unset($_SESSION['login_errors']);
-	session_destroy();
-?>
