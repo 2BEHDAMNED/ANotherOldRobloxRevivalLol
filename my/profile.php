98c71a6720a7d122110128e2acccb7288f60a4f6
Zlysie <realoikmo@gmail.com>
2025-09-09 07:21:32 +0100

    Create profile page

---
+++ my/profile.php
@@ -0,0 +1,66 @@
+<?php
+	session_start();
+
+	require_once $_SERVER['DOCUMENT_ROOT'].'/core/utilities/userutils.php';
+	$user = UserUtils::RetrieveUser();
+
+	if($user == null) {
+		die(header("Location: /"));
+	}
+
+
+	if(isset($_POST['ANORRL$Home$Status$Text']) &&
+	   isset($_POST['ANORRL$Home$Status$Submit'])) {
+		$result = Status::Send($user->id, trim($_POST['ANORRL$Home$Status$Text']));
+
+		if($result['error']) {
+			$_SESSION['ANORRL$Home$StatusError'] = true;
+			$_SESSION['ANORRL$Home$StatusResult'] = $result['reason'];
+		} else {
+			$_SESSION['ANORRL$Home$StatusError'] = false;
+			$_SESSION['ANORRL$Home$StatusResult'] = "Success!";
+		}
+
+		die(header("Location: /my/home"));
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Profile - ANORRL</title>
+		<link rel="icon" type="image/x-icon" href="/favicon.ico">
+		<link rel="stylesheet" href="/css/AllCSS.css">
+		<script src="/js/jquery.js"></script>
+		<script src="/js/main.js"></script>
+		<style>
+
+		</style>
+	</head>
+	<body>
+		<table id="FeedItem" class="template">
+			<td class="User">
+				<a href="">
+					<img src="/images/avatar.png">
+					<div id="Name">Name here</div>
+				</a>
+			</td>
+			<td id="Content">
+				<code>Content content</code>
+				<div id="DatePosted">Posted <span id="Date">DD/MM/YYYY</span> ago</div>
+			</td>
+		</table>
+		<div id="Container">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/header.php'; ?>
+			<div id="Body">
+				<div id="BodyContainer">
+                    
+				</div>
+				<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/footer.php'; ?>
+			</div>
+		</div>
+	</body>
+</html>
+<?php
+	unset($_SESSION['ANORRL$Home$StatusError']);
+	unset($_SESSION['ANORRL$Home$StatusResult']);
+?>
