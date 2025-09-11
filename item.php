227de0b622f5d73ce88cdfbe211967265607fdd4
zlysie <60061261@nrc.ac.uk>
2025-09-11 12:12:43 +0100

    Work on item page

--- item.php
+++ item.php
@@ -8,7 +8,7 @@ require_once $_SERVER['DOCUMENT_ROOT']."/core/asset.php";
 $asset = Asset::FromID($id);
 
 if($asset != null) {
-    $new_name = preg_replace('/^[\w]*$/', "", $asset->name);
+    $new_name = preg_replace('/[^A-Za-z0-9]/', "", $asset->name);
     $new_name = strtolower($new_name);
     $new_name = str_replace(" ", "-", $new_name);
     
@@ -16,8 +16,25 @@ if($asset != null) {
         die(header("Location: /$new_name-item?id=$id"));
     }
 }
-
-//
-
-
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page - ANORRL</title>
+		<link rel="icon" type="image/x-icon" href="/favicon.ico">
+		<link rel="stylesheet" href="/css/AllCSS.css?t=<?= time() ?>">
+		<script src="/js/jquery.js"></script>
+		<script src="/js/main.js"></script>
+	</head>
+	<body>
+		<div id="Container">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/header.php'; ?>
+			<div id="Body">
+				<div id="BodyContainer">
+					
+				</div>
+				<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/footer.php'; ?>
+			</div>
+		</div>
+	</body>
+</html>
