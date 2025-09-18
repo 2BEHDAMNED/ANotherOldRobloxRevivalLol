2dc1b3ccfbf95c6e67b44d2b75fe3a973195f168
Zlysie <realoikmo@gmail.com>
2025-09-18 18:46:01 +0100

    profiler title name fix lol

--- users/profile.php
+++ users/profile.php
@@ -40,7 +40,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
-		<title><?= $user->name ?> - ANORRL</title>
+		<title><?= $get_user->name ?> - ANORRL</title>
 		<link rel="icon" type="image/x-icon" href="/favicon.ico">
 		<link rel="stylesheet" href="/css/AllCSS.css?t=<?= time() ?>">
 		<script src="/js/jquery.js"></script>
