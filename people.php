f4123f4b1108abd0e1004628a93a0bb174441ac1
Zlysie <realoikmo@gmail.com>
2025-09-22 00:57:56 +0100

    Iota to ANORRL

--- people.php
+++ people.php
@@ -9,7 +9,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
-		<title>People - Iota</title>
+		<title>People - ANORRL</title>
 		<link rel="icon" type="image/x-icon" href="/favicon.ico">
 		<link rel="stylesheet" href="/css/AllCSS.css?t=<?= time() ?>">
 		<script src="/js/jquery.js"></script>
