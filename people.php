9518049a890878a991770b55ebded5fb8fd845c9
Zlysie <realoikmo@gmail.com>
2025-09-22 00:50:14 +0100

    Basic users page done

--- people.php
+++ people.php
@@ -16,11 +16,6 @@
 		<script src="/js/jquery.imageloader.js"></script>
 		<script src="/js/main.js"></script>
 		<style>
-			#Users #SearchBox,#Submit {
-				border-radius: 4px;
-				border: 1px solid gray;
-				padding:3px;
-			}
 
 			#Users #SearchBox {
 				width: 400px
@@ -31,18 +26,53 @@
 			}
 
 			#Users form {
-				margin: 0 auto;
+				margin: 5px auto;
+				margin-bottom: 15px;
+				text-align: center; 
+			}
+
+			#Users table {
+				border: 2px solid black;
+				background: #222;
+				padding: 5px;
+				border-collapse: separate;
+			}
+
+			#Users tr {
+				border-bottom: 1px solid black;
+			}
+
+			h2 {
+				margin: 0;
+				margin-top: 10px;
+			}
+
+			#Users {
+				border: 2px solid black;
+				background: #2a2a2a;
+				padding: 10px;
+			}
+
+			#UsersNavLinks {
 				text-align: center;
+				margin: 15px;
+				margin-bottom: 5px;
+				display: none;
+			}
+
+			#FormPanel {
+				display: none;
 			}
 		</style>
 	</head>
 	<body>
 		<div id="Container">
-			<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/header.php'; ?>
-			<div id="WrapperBody">
-				<div id="Body">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/header.php'; ?>
+			<div id="Body">
+				<div id="BodyContainer">
+					<h2>People</h2>
 					<div id="Users">
-						<form method="GET">
+						<form method="GET" id="FormPanel">
 							<input id="SearchBox" name="query" type="text" placeholder="Look for users lol">
 							<input id="Submit" type="submit" value="Search">
 						</form>
@@ -53,18 +83,32 @@
 								<th style="border:0; width: 600px; max-width: 600px;">Blurb</th>
 								<th width="150" style="border:0">Active</th>
 							</tr>
-							<tr>
-								<td style="text-align: center"><a href="/users/1/profile" title="Zlysie"><img src="/images/avatar.png" width="64"></a></td>
-								<td><img src="/images/OnlineStatusIndicator_IsOffline.gif"> <a href="/users/1/profile">Zlysie</a></td>
-								<td style="word-break: break-all;">Blurb</td>
-								<td style="text-align: center">Online</td>
-							</tr>
+							<?php 
+								$users = UserUtils::GetLatestUsers(50);
+
+								foreach($users as $user) {
+									$userid = $user->id;
+									$username = $user->name;
+									$userbio = $user->blurb;
+									if(trim($userbio) == "")  {
+										$userbio = "<b>No blurb set</b>";
+									}
+									echo <<<EOT
+									<tr>
+										<td style="text-align: center"><a href="/users/$userid/profile" title="$username"><img src="/images/avatar.png" width="64"></a></td>
+										<td style="text-align: center"><img src="/images/OnlineStatusIndicator_IsOffline.gif"> <a href="/users/$userid/profile">$username</a></td>
+										<td style="word-break: break-all;">$userbio</td>
+										<td style="text-align: center">Online</td>
+									</tr>
+									EOT;
+								}
+							?>
+							
 						</table>
 						<div id="UsersNavLinks">
-							<a id="Back" href="">&lt;</a><a href="">1</a><a href="">2</a><a href="">3</a><a href="">4</a><a href="">5</a><a id="Next" href="">&gt;</a>
+							<a id="Back" href="">&lt;&lt; Back</a> <input type="number"> of 1 <a id="Next" href="">Next &gt;&gt;</a>
 						</div>
 					</div>
-					
 				</div>
 				<?php include $_SERVER['DOCUMENT_ROOT'].'/core/ui/footer.php'; ?>
 			</div>
