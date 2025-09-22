c5f06c5afd1d5fa84c763b697d43fd19652309de
Zlysie <realoikmo@gmail.com>
2025-09-22 13:09:07 +0100

    Update no badges message

--- users/profile.php
+++ users/profile.php
@@ -468,7 +468,7 @@
 										
 									if($count == 0): ?>
 									<tr>
-										<td class="Loading">Loading badges...</td>
+										<td class="Loading"><?=-$get_user->name ?> has no badges!</td>
 									</tr>
 									<?php endif ?>
 								</table>
