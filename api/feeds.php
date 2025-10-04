9ef8b71da4e1cc5c66613cc4fccb121870c5d2bf
Zlysie <realoikmo@gmail.com>
2025-10-04 16:00:28 +0100

    ago ago fix on home feeds

--- api/feeds.php
+++ api/feeds.php
@@ -31,7 +31,7 @@
 		}
 
 		if (!$full) $string = array_slice($string, 0, 1);
-		return $string ? implode(', ', $string) . ' ago' : 'just now';
+		return $string ? implode(', ', $string) . '' : 'just now';
 	}
 
 	if($user != null) {
