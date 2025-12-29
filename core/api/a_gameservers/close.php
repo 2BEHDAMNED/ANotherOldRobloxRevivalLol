cdb8c7739a5cd2c487d451a5339dbdfdc6f48ea5
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2025-12-29 18:17:04 +0000

    diff lib test

--- core/api/a_gameservers/close.php
+++ core/api/a_gameservers/close.php
@@ -1,6 +1,6 @@
 <?php
 
-	require_once $_SERVER['DOCUMENT_ROOT']."/core/renderer.php";
+	require_once $_SERVER['DOCUMENT_ROOT']."/core/rcclib.php";
 
 	$settings = parse_ini_file($_SERVER['DOCUMENT_ROOT']."/core/settings.env", true);
 	
@@ -9,11 +9,11 @@
 	$access = $settings['asset']['ACCESSKEY'];
 	$rcc_ip = $rcc_settings['RCCGAMEIP'];
 	$rcc_port = 64898;
-
+	
 	if(isset($_GET['access']) && isset($_GET['jobID'])) {
 		if($_GET['access'] == $access) {
-			$rcc = new Roblox\Grid\Rcc\RCCServiceSoap($rcc_ip, $rcc_port);
-			$base64data = $rcc->CloseJob($_GET['jobID']);
+			$rcc= new RCCServiceSoap($rcc_ip, $rcc_port);
+			$rcc->CloseJob($_GET['jobID']."-GameScript");
 		}
 		
 	}
