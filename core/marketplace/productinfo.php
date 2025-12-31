7fd7f3a44692b0d2a7d7f9984b2aaf77415cc342
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2025-12-31 13:45:52 +0000

    product info finished

--- core/marketplace/productinfo.php
+++ core/marketplace/productinfo.php
@@ -1,39 +1,49 @@
 <?php
-header('Content-type: application/json');
-if(isset($_GET['productId'])) {
-	$assetid = intval($_GET['productId']);
-} else if(isset($_GET['assetId'])) {
-	$assetid = intval($_GET['assetId']);
-} else {
-	die();
-}
+	header('Content-type: application/json');
+	if(isset($_GET['productId'])) {
+		$assetid = intval($_GET['productId']);
+	} else if(isset($_GET['assetId'])) {
+		$assetid = intval($_GET['assetId']);
+	} else {
+		die();
+	}
+
+	require_once $_SERVER['DOCUMENT_ROOT']."/core/asset.php";
+
+	$asset = Asset::FromID($assetid);
+
+	if($asset == null) {
+		die();
+	}
+
+	die(json_encode([
+		"TargetId" => $assetid,
+		"ProductType" => "User Product",
+		"AssetId" => $assetid,
+		"ProductId" => $assetid,
+		"Name" => $asset->name,
+		"Description" => $asset->description,
+		"AssetTypeId" => $asset->type->ordinal(),
+		"Creator" => [
+			"Id" => $asset->creator->id,
+			"Name" => $asset->creator->name,
+			"CreatorType" => "User",
+			"CreatorTargetId" => $asset->creator->id
+		],
+		"IconImageAssetId" => $assetid,
+		"Created" => "2007-05-30T07:05:24.057Z",
+		"Updated" => "2013-08-06T17:49:26.167Z",
+		"PriceInRobux" => 0,
+		"PremiumPriceInRobux" => 0,
+		"PriceInTickets" => null,
+		"IsNew" => false,
+		"IsForSale" => false,
+		"IsPublicDomain" => true,
+		"IsLimited" => false,
+		"IsLimitedUnique" => false,
+		"Remaining" => null,
+		"Sales" => null,
+		"MinimumMembershipLevel" => 0
+	]));
+
 ?>
-{
-	"TargetId": <?php echo($assetid); ?>,
-	"ProductType": "User Product",
-	"AssetId": <?php echo($assetid); ?>,
-	"ProductId": <?php echo($assetid); ?>,
-	"Name":"test",
-	"Description":"testing product",
-	"AssetTypeId":9,
-	"Creator": {
-		"Id": 1,
-		"Name": "Roblox",
-		"CreatorType": "User",
-		"CreatorTargetId": 1
-	},
-	"IconImageAssetId": <?php echo($assetid); ?>,
-	"Created": "2007-05-30T07:05:24.057Z",
-	"Updated": "2013-08-06T17:49:26.167Z",
-	"PriceInRobux": 0,
-	"PremiumPriceInRobux": 0,
-	"PriceInTickets": null,
-	"IsNew": false,
-	"IsForSale": false,
-	"IsPublicDomain": true,
-	"IsLimited": false,
-	"IsLimitedUnique": false,
-	"Remaining": null,
-	"Sales": null,
-	"MinimumMembershipLevel": 0
-}
