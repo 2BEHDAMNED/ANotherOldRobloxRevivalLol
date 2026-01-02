b1f5949738538bc3a3e27ab9e773248d52fb2ee4
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-02 13:25:49 +0000

    lord help me

--- core/asset.php
+++ core/asset.php
@@ -463,56 +463,26 @@
 		
 		public static function GetAssetsOfTypePaged(string $query, AssetType $type, int $pagenum, int $count) {
 			include $_SERVER["DOCUMENT_ROOT"]."/core/connection.php";
-			if(strlen(trim($query)) != 0) {
-				$stmt_getuser = $con->prepare("SELECT * FROM `assets` WHERE `asset_name` LIKE ? AND `asset_type` = ? AND `asset_public` = 1 ORDER BY `asset_lastedited` LIMIT ?, ?");
-				
-				$page = (($pagenum-1)*$count);
-				$q = "%$query%";
-				$ordinal = $type->ordinal();
-				$stmt_getuser->bind_param('siii', $q, $ordinal, $page, $count);
-				$stmt_getuser->execute();
+			$stmt_getuser = $con->prepare("SELECT asset_places.* FROM `asset_places`, `assets` WHERE assets.asset_id = asset_places.place_id AND assets.asset_public = 1 AND assets.asset_name LIKE ? ORDER BY `place_currently_playing` DESC, `asset_lastedited` DESC LIMIT ?, ?");
+			$page = (($pagenum-1)*$count);
+			$q = "%$query%";
+			$stmt_getuser->bind_param('sii', $q, $page, $count);
+			$stmt_getuser->execute();
 
-				$result = $stmt_getuser->get_result();
+			$result = $stmt_getuser->get_result();
 
-				$result_array = [];
+			$result_array = [];
 
-				if($result->num_rows != 0) {
-					while($row = $result->fetch_assoc()) {
-						$asset = new Asset($row);
-						if($row['asset_type'] == AssetType::PLACE->ordinal()) {
-							$asset = Place::FromID($row['asset_id']);
-						} else {
-							$asset = new Asset($row);
-						}
+			if($result->num_rows != 0) {
+				while($row = $result->fetch_assoc()) {
+					$asset = Place::FromID($row['place_id']);
 
-						if(!$asset->notcatalogueable && $asset->public) {
-							array_push($result_array, $asset);
-						}
-						
+					if(!$asset->notcatalogueable && $asset->public) {
+						array_push($result_array, $asset);
 					}
-					return $result_array;
+					
 				}
-			} else {
-				$stmt_getuser = $con->prepare("SELECT asset_places.* FROM `asset_places`, `assets` WHERE assets.asset_id = asset_places.place_id AND assets.asset_public = 1 ORDER BY `place_currently_playing` DESC, `asset_lastedited` DESC LIMIT ?, ?");
-				$page = (($pagenum-1)*$count);
-				$stmt_getuser->bind_param('ii', $page, $count);
-				$stmt_getuser->execute();
-
-				$result = $stmt_getuser->get_result();
-
-				$result_array = [];
-
-				if($result->num_rows != 0) {
-					while($row = $result->fetch_assoc()) {
-						$asset = Place::FromID($row['place_id']);
-
-						if(!$asset->notcatalogueable && $asset->public) {
-							array_push($result_array, $asset);
-						}
-						
-					}
-					return $result_array;
-				}
+				return $result_array;
 			}
 
 			return [];
@@ -520,51 +490,26 @@
 
 		public static function GetAssetsOfType(string $query, AssetType $type) {
 			include $_SERVER["DOCUMENT_ROOT"]."/core/connection.php";
-			if(strlen(trim($query)) != 0) {
-				$stmt_getuser = $con->prepare("SELECT * FROM `assets` WHERE `asset_name` LIKE ? AND `asset_type` = ? AND `asset_public` = 1");
+
+			$stmt_getuser = $con->prepare("SELECT asset_places.* FROM `asset_places`, `assets` WHERE assets.asset_id = asset_places.place_id AND assets.asset_public = 1 AND assets.asset_name LIKE ? ORDER BY `place_currently_playing` DESC, `asset_lastedited` DESC;");
 			
-				$q = "%$query%";
-				$ordinal = $type->ordinal();
-				$stmt_getuser->bind_param('si', $q, $ordinal);
-				$stmt_getuser->execute();
+			$q = "%$query%";
+			$stmt_getuser->bind_param('s', $q);
+			$stmt_getuser->execute();
 
-				$result = $stmt_getuser->get_result();
+			$result = $stmt_getuser->get_result();
 
-				$result_array = [];
+			$result_array = [];
 
-				if($result->num_rows != 0) {
-					while($row = $result->fetch_assoc()) {
-						if($row['asset_type'] == AssetType::PLACE->ordinal()) {
-							$asset = Place::FromID($row['asset_id']);
-						} else {
-							$asset = new Asset($row);
-						}
-
-						if(!$asset->notcatalogueable && $asset->public) {
-							array_push($result_array, $asset);
-						}
+			if($result->num_rows != 0) {
+				while($row = $result->fetch_assoc()) {
+					$asset = Place::FromID($row['place_id']);
+					if(!$asset->notcatalogueable && $asset->public) {
+						array_push($result_array, $asset);
 					}
-					return $result_array;
 				}
-			} else {
-				$stmt_getuser = $con->prepare("SELECT asset_places.* FROM `asset_places`, `assets` WHERE assets.asset_id = asset_places.place_id AND assets.asset_public = 1 ORDER BY `place_currently_playing` DESC, `asset_lastedited` DESC;");
-				$stmt_getuser->execute();
-
-				$result = $stmt_getuser->get_result();
-
-				$result_array = [];
-
-				if($result->num_rows != 0) {
-					while($row = $result->fetch_assoc()) {
-						$asset = Place::FromID($row['place_id']);
-						if(!$asset->notcatalogueable && $asset->public) {
-							array_push($result_array, $asset);
-						}
-					}
-					return $result_array;
-				}
+				return $result_array;
 			}
-			
 
 			return [];
 		}
