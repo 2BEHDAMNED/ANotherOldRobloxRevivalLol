1587a74116bbc5f112bf61500440205749a47416
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-02 18:07:03 +0000

    the thumbnail system required user login

--- core/thumbs/index.php
+++ core/thumbs/index.php
@@ -44,45 +44,44 @@
 		$asset = Asset::FromID($id);
 		if($asset != null) {
 			include $_SERVER['DOCUMENT_ROOT']."/core/connection.php";
-			
-			if($user != null) {
+
 				
-				$stmt = $con->prepare('SELECT * FROM `assetversions` WHERE `version_assetid` = ? ORDER BY `version_id` DESC');
-				$stmt->bind_param('i', $id);
-				$stmt->execute();
+			$stmt = $con->prepare('SELECT * FROM `assetversions` WHERE `version_assetid` = ? ORDER BY `version_id` DESC');
+			$stmt->bind_param('i', $id);
+			$stmt->execute();
 
-				$stmt_result = $stmt->get_result();
+			$stmt_result = $stmt->get_result();
 
-				$md5hash = $stmt_result->fetch_assoc()['version_md5thumb'];
+			$md5hash = $stmt_result->fetch_assoc()['version_md5thumb'];
 
-				if($md5hash == "sound" && $asset->type == AssetType::AUDIO) {
-					$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/audio.png");
-				} else if($md5hash == "script" && $asset->type == AssetType::LUA) {
-					$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/script.png");
-				} else if($md5hash == "animation" && $asset->type == AssetType::ANIMATION) {
-					$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/animation.png");
-				} else {
-					if($asset->relatedasset != null || $asset->type == AssetType::IMAGE) {
-						if(file_exists($_SERVER['DOCUMENT_ROOT']."/../assets/$md5hash")) {
-							$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/../assets/$md5hash");
-							$specialcase = true;
-						} else {
-							$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/unavailable.jpg");
-						}
+			if($md5hash == "sound" && $asset->type == AssetType::AUDIO) {
+				$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/audio.png");
+			} else if($md5hash == "script" && $asset->type == AssetType::LUA) {
+				$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/script.png");
+			} else if($md5hash == "animation" && $asset->type == AssetType::ANIMATION) {
+				$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/animation.png");
+			} else {
+				if($asset->relatedasset != null || $asset->type == AssetType::IMAGE) {
+					if(file_exists($_SERVER['DOCUMENT_ROOT']."/../assets/$md5hash")) {
+						$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/../assets/$md5hash");
+						$specialcase = true;
 					} else {
-						if(file_exists($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$id")) {
-							$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$id");
-						}
-						else if(file_exists($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$md5hash")) {
-							$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$md5hash");
-						}
-						else {
-							$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/unavailable.jpg");
-						}
+						$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/unavailable.jpg");
 					}
-					
+				} else {
+					if(file_exists($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$id")) {
+						$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$id");
+					}
+					else if(file_exists($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$md5hash")) {
+						$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/../assets/thumbs/$md5hash");
+					}
+					else {
+						$contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/unavailable.jpg");
+					}
 				}
+				
 			}
+			
 			
 
 			ob_clean();
