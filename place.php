9a1810f44ad289968822d70bb5ee8a7cad9a2faa
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-02 18:06:25 +0000

    WHY WONT THE IMAGE LOAD HUH?

--- place.php
+++ place.php
@@ -62,7 +62,7 @@ $header_data = $asset;
 		<meta property="og:description" content="<?= htmlspecialchars(substr($asset->description, 0, 128), ENT_QUOTES) ?>">
 		<meta property="og:url" content="https://arl.lambda.cam/<?= $asset->GetURLTitle()?>-place?id=<?= $asset->id ?>">
 		<meta property="og:site_name" content="ANORRL">
-		<meta property="og:image" content="https://arl.lambda.cam/thumbs/?id=<?= $asset->id ?>"/>
+		<meta property="og:image" content="https://arl.lambda.cam/thumbs/?id=<?= $asset->id ?>">
 		<?php if($user == null) { die(); } ?>
 		<script src="/js/jquery.js"></script>
 		<script src="/js/main.js?t=<?= time() ?>"></script>
