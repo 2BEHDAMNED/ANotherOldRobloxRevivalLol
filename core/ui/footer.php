cc0c98191f6384281d8c566322e1da67422b33d2
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-05 04:31:29 +0000

    footer styling fix

--- core/ui/footer.php
+++ core/ui/footer.php
@@ -1,8 +1,8 @@
 <div id="Footer" style="position:relative">
 	<div id="FooterContainer">
 		<p id="Legalese">
-			<img src="/images/hankdanceloop.gif" style="height: 60px;float:left;margin-top:-13px;">
-			<span style="margin-top: 25px;display: block;">
+			<img src="/images/hankdanceloop.gif" style="height: 60px;float:left;margin-top:-8px;">
+			<span style="margin-top: 20px;display: block;">
 				ANORRL (ANother Old Roblox Revival Lol) is made for a friends-only userbase<br>and thus we won't be accepting anyone that we don't know directly.
 				<br><b>Don't leak the url for this too btw :3</b>
 			</span>
