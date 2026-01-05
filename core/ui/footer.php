8c76e83a691b9df53bff80b7b05d3ffde5b05229
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-05 04:32:35 +0000

    changed bold text on footer

--- core/ui/footer.php
+++ core/ui/footer.php
@@ -4,7 +4,7 @@
 			<img src="/images/hankdanceloop.gif" style="height: 60px;float:left;margin-top:-8px;">
 			<span style="margin-top: 20px;display: block;">
 				ANORRL (ANother Old Roblox Revival Lol) is made for a friends-only userbase<br>and thus we won't be accepting anyone that we don't know directly.
-				<br><b>Don't leak the url for this too btw :3</b>
+				<br><b>Made by grace with love &lt;3</b>
 			</span>
 			<br style="display:block;clear:both;">
 		</p>
