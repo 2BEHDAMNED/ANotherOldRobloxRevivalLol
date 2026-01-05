6e70e1a6864bdc19887a81f284c2c0ea180f27cd
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-05 04:43:16 +0000

    scratch that revert it back to before i removed friends

--- my/home.php
+++ my/home.php
@@ -32,7 +32,7 @@
 		<link rel="stylesheet" href="/css/AllCSS.css?t=<?= time() ?>">
 		<script src="/js/jquery.js"></script>
 		<script src="/js/main.js?t=<?= time() ?>"></script>
-		<script src="/js/home.js?t=12282025_2"></script>
+		<script src="/js/home.js"></script>
 		<style>
 			h1, h2, h3, h4 {
 				width: initial\9;
@@ -43,7 +43,7 @@
 		<table id="FeedItem" template>
 			<td class="User">
 				<a href="">
-					<img src="/thumbs/player?id=">
+					<img src="">
 					<div id="Name">Name here</div>
 				</a>
 			</td>
@@ -75,62 +75,77 @@
 											<span id="NoStatus">Seems like you have no status... Try sending one!</span>
 										EOT;
 									}
-								?>			
-								</div>
-								<div id="Favourites">
-									<h3>Favourites</h3>
-									<div id="Games">
-										<!--<div class="Game">
-											<a href="/game/1000" title="Whats up guys im gonna rob a store">
-												<img src="/images/review-pending.png">
-												<span id="Name">aaaaaaaaaaaaaaaaaaaaa...</span>
-											</a>
-											<div id="Stats">
-												<div id="OnlinePlayers">Couple ppl online</div>
-												<div id="Created">Creator: <a href="/profile/1">Creator</a></div>
-											</div>
-										</div>-->
-										<span id="NoTagline">No favourites yet!</span>
-									</div>
-								</div>
-								<div id="RecentlyPlayed">
-									<h3>Recently Played</h3>
-									<div id="Games">
-										<!--<div class="Game">
-											<a href="/game/1000" title="Whats up guys im gonna rob a store">
-												<img src="/images/review-pending.png">
-												<span id="Name">aaaaaaaaaaaaaaaaaaaaa...</span>
-											</a>
-											<div id="Stats">
-												<div id="OnlinePlayers">Couple ppl online</div>
-												<div id="Created">Creator: <a href="/profile/1">Creator</a></div>
-											</div>
-										</div>-->
-										<span id="NoTagline">Haven't played anything yet!</span>
-									</div>
-								</div>
+								?>
+								
 							</div>
-							<div id="FeedsContainer">
-								<h2>Your feed</h2>
-								<div id="Submit">
-									<?php if(isset($_SESSION['ANORRL$Home$StatusError'])): ?>
-									<?php if($_SESSION['ANORRL$Home$StatusError']): ?>
-									<div class="Error"><?= $_SESSION['ANORRL$Home$StatusResult'] ?></div>
-									<?php else: ?>
-									<div class="Success">Success!</div>
-									<?php endif ?>
-									<?php endif ?>
-									<form method="POST">
-										<input name="ANORRL$Home$Status$Text" type="text" minlength="4" maxlength="64" placeholder="What are you feeling today?">
-										<input name="ANORRL$Home$Status$Submit" type="submit" value="Submit Status">
-									</form>
-								</div>
-								<div id="Feeds">
+							</div>
+							<div id="FriendsContainer">
+								<h3>Friends<?php if($user->GetFriendsCount() > 5): ?> <a href="/my/friends" style="font-size: 12px;">(See all)</a><?php endif ?></h3>
+								<?php if($user->GetFriendsCount() != 0): ?>
+								<ul id="Friends">
+									<li class="Friend">
+										<a id="ProfileLink" href="/users/1/profile">
+											<img id="Profile" src="/images/avatar.png">
+											<div id="Name">Username</div>
+										</a>
+									</li>
+								</ul>
+								<?php else: ?>
+								<ul id="Friends" style="display: table">
+									<div id="NoFriends">You don't have any friends!</div>
+								</ul>
+								<?php endif ?>
+							</div>
+							<br style="clear: both">
+							<div id="FeedAndGames">
+								<div id="ProfileGames">
+									<div id="RecentlyPlayed">
+										<h3>Recently Played</h3>
+										<div id="Games">
+											<!--<div class="Game">
+												<a href="/game/1000" title="Whats up guys im gonna rob a store">
+													<img src="/images/review-pending.png">
+													<span id="Name">aaaaaaaaaaaaaaaaaaaaa...</span>
+												</a>
+												<div id="Stats">
+													<div id="OnlinePlayers">Couple ppl online</div>
+													<div id="Created">Creator: <a href="/profile/1">Creator</a></div>
+												</div>
+											</div>-->
+
+											<span id="NoTagline">No recently played games yet!</span>
+										</div>
+									</div>
+									<div id="Favourites">
+										<h3>Favourites</h3>
+										<div id="Games">
+											<span id="NoTagline">No favourites yet!</span>
+										</div>
+									</div>
 									
 								</div>
-								<div id="Pager" style="display:none">
-									<a href="javascript:ANORRL.Home.DeadvanceFeed()" id="BackPager">&lt;&lt; Back</a> <span id="PageCounter">Page 1 of 1</span> <a href="javascript:ANORRL.Home.AdvanceFeed()" id="NextPager">Next &gt;&gt;</a>
-								</div>	
+								<div id="FeedsContainer">
+									<h2>Your feed</h2>
+									<div id="Submit">
+										<?php if(isset($_SESSION['ANORRL$Home$StatusError'])): ?>
+										<?php if($_SESSION['ANORRL$Home$StatusError']): ?>
+										<div class="Error"><?= $_SESSION['ANORRL$Home$StatusResult'] ?></div>
+										<?php else: ?>
+										<div class="Success">Success!</div>
+										<?php endif ?>
+										<?php endif ?>
+										<form method="POST">
+											<input name="ANORRL$Home$Status$Text" type="text" minlength="4" maxlength="64" placeholder="What are you feeling today?">
+											<input name="ANORRL$Home$Status$Submit" type="submit" value="Submit Status">
+										</form>
+									</div>
+									<div id="Feeds">
+										
+									</div>
+									<div id="Pager" style="display:none">
+										<a href="javascript:ANORRL.Home.DeadvanceFeed()" id="BackPager">&lt;&lt; Back</a> <span id="PageCounter">Page 1 of 1</span> <a href="javascript:ANORRL.Home.AdvanceFeed()" id="NextPager">Next &gt;&gt;</a>
+									</div>	
+								</div>
 							</div>
 						</div>
 					</div>
