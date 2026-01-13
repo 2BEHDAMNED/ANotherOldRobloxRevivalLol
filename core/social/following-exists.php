90dc327e19a0ec7f898ea8013faf878450878078
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-13 22:28:15 +0000

    dont cache and json?

--- core/social/following-exists.php
+++ core/social/following-exists.php
@@ -1,6 +1,11 @@
 <?php
 	require_once $_SERVER['DOCUMENT_ROOT']."/core/utilities/userutils.php";
 	header("Content-Type: application/json");
+
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+
 	if(isset($_GET['userId']) && isset($_GET['followerUserId'])) {
 		$user = User::FromID(intval($_GET['userId']));
 		$userToCheck = User::FromID(intval($_GET['followerUserId']));
--- core/social/unfollow.php
+++ core/social/unfollow.php
@@ -4,6 +4,7 @@
 	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
+	header("Content-Type: application/json");
 
 	$user = UserUtils::RetrieveUser();
 
@@ -28,7 +29,7 @@
 			}
 		}
 	}
-
+	http_response_code(420);
 	die(json_encode(
 		[
 			"success" => false
