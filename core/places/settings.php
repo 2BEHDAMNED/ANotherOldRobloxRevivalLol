24f530753c3883eb9c5b32735b0175e367f9f1e9
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-19 22:52:44 +0000

    place settings fix

--- core/places/settings.php
+++ core/places/settings.php
@@ -1,9 +1,23 @@
 <?php
 header('Content-type: application/json');
-?>
-{
-	"Creator":1,
-	"Asset":2,
-	"TypeId": 1,
-	"AssetTypeId": 9
+require_once $_SERVER['DOCUMENT_ROOT']."/core/classes/asset.php";
+
+$place = Place::FromID(intval($_GET['placeId']));
+
+if($place != null) {
+	echo json_encode([
+		"AssetId" => $place->id,
+		"ID" => $place->id,
+		"Creator" => [
+			"Id" => $place->creator->id,
+			"Name" => $place->creator->name,
+			"CreatorTargetId" => $place->creator->id,
+			"CreatorType" => 0
+		],
+		
+	]);
+} else {
+	echo "{}";
 }
+
+?>
