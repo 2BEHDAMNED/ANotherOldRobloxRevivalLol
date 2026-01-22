3cc7a2f932af22c543b08bc8ab1c3afdbe60f28f
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-01-22 02:51:54 +0000

    internal ios app test??

--- core/authentication/getallowedsecurityversions.php
+++ core/authentication/getallowedsecurityversions.php
@@ -1,2 +1,2 @@
 <?php header("content-type:application/json"); ?>
-{"data":["0.235.0pcplayer", "0.285.0pcplayer", "0.300.0pcplayer","2.235.0androidapp","2.237.0macplayer"]}
+{"data":["0.235.0pcplayer", "0.285.0pcplayer", "0.300.0pcplayer","2.235.0androidapp","2.237.0macplayer","INTERNALiosapp"]}
