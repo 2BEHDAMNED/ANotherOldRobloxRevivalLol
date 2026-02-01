ff6427d95b8e9bd27ef1d8e7728c6fa3f053c246
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-02-01 21:29:16 +0000

    thumbsnails testing

--- core/thumbs/rawasset.php
+++ core/thumbs/rawasset.php
@@ -1,10 +1,19 @@
 <?php
 
-    // Width=60&Height=62&ImageFormat=png&AssetID=8
+	// Width=60&Height=62&ImageFormat=png&AssetID=8
 
-    $width = $_GET['Width'];
-    $height = $_GET['Height'];
-    $assetid = $_GET['AssetID'];
+	
 
-    echo "/thumbs/?id=$assetid&sx=$width&sy=$height";
+	if(isset($_GET['format']) && $_GET['format'] == "png") {
+		
+		die(header("Location: /thumbs/?id="/$_GET['assetId']));
+
+	} else {
+		$width = $_GET['Width'];
+		$height = $_GET['Height'];
+		$assetid = $_GET['AssetID'];
+		echo "/thumbs/?id=$assetid&sx=$width&sy=$height";
+	}
+
+	
 ?>
