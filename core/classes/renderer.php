9680154c8fda9d7d3c6dd1adbbf9a9979215fad3
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-02-04 07:08:35 +0000

    ok maybe gear render fix

--- core/classes/renderer.php
+++ core/classes/renderer.php
@@ -115,6 +115,16 @@
 				player.CharacterAppearance = "http://$domain/Asset/CharacterFetch.ashx?userId=$id"
 				player:LoadCharacter(false)
 
+				if player.Character then
+					for _, child in pairs(player.Character:GetChildren()) do
+						if child:IsA("Tool") then
+							player.Character.Torso["Right Shoulder"].CurrentAngle = math.rad(90)
+							break
+						end
+					end
+				end
+
+
 				return  (game:GetService("ThumbnailGenerator"):Click("PNG", 420, 420, true))
 				EOT;
 
