e7ec405fbf00d7749ee050be8dffd45b4f3d2680
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-02-11 20:20:29 +0000

    REMOVE THAT

--- core/gamescripts/placelauncher.php
+++ core/gamescripts/placelauncher.php
@@ -283,7 +283,7 @@
 						<<<EOT
 						game:SetServerSaveUrl("http://arl.lambda.cam/Data/Upload.ashx?assetid=$placeId&access=$access")
 						print("SETTINGS SAVE URL TO http://arl.lambda.cam/Data/Upload.ashx?assetid=$placeId&access=$access")
-						print(game:GetServerSaveUrl())
+						--print(game:GetServerSaveUrl())
 						loadfile("http://arl.lambda.cam/game/maingameserver.ashx")($placeId, $port, "http://arl.lambda.cam", "$access", "$jobId", true)
 						EOT);
 						$base64data = $rcc->OpenJob($job, $script);
