4e25a9c0637531fc51b6ef0a4747ec51f627be1a
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-02-12 01:23:24 +0000

    icons to profile

--- users/profile.php
+++ users/profile.php
@@ -70,7 +70,7 @@
 		<link rel="stylesheet" href="/css/new/main.css">
 		<link rel="stylesheet" href="/css/new/comments.css?v=1">
 		<link rel="stylesheet" href="/css/new/stuff.css?v=1">
-		<link rel="stylesheet" href="/css/new/my/profile.css?v=5">
+		<link rel="stylesheet" href="/css/new/my/profile.css?v=6">
 		<script src="/js/core/jquery.js"></script>
 		<script src="/js/main.js?t=<?= time() ?>"></script>
 		<script src="/js/placelauncher.js?t=<?= time() ?>"></script>
