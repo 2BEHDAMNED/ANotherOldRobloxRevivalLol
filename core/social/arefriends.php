ea98d08f003bac547c8e5ea22606b27e7a6820dc
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-02-17 19:02:32 +0000

    PLAIN TEXT HEADER.

--- core/social/arefriends.php
+++ core/social/arefriends.php
@@ -3,6 +3,7 @@
 	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
+	header("Content-Type: text/plain");
 
 	require_once $_SERVER['DOCUMENT_ROOT']."/core/classes/user.php";
 
