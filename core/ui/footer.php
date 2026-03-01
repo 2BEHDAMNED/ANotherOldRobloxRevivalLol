95e1797ff4b2e91c1fc188a9e7a141dc30c059a1
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-03-01 23:40:03 +0000

    footer fix i guess

--- core/ui/footer.php
+++ core/ui/footer.php
@@ -1,10 +1,10 @@
-<div id="Footer" style="position:relative">
-	<div id="FooterContainer">
-		<p id="Legalese">
+<div id="Footer" style="position:relative; font-family: Arial,Helvetica,sans-serif !important">
+	<div id="FooterContainer" style=" font-family: Arial,Helvetica,sans-serif !important">
+		<p id="Legalese" style=" font-family: Arial,Helvetica,sans-serif !important">
 			<img src="/images/hankdanceloop.gif" style="height: 60px;float:left;margin-top:-8px;">
-			<span style="margin-top: 20px;display: block;">
+			<span style="margin-top: 20px;display: block; font-family: Arial,Helvetica,sans-serif !important">
 				ANORRL (ANother Old Roblox Revival Lol) is made for a friends-only userbase<br>and thus we won't be accepting anyone that we don't know directly.
-				<br><b>Made by grace with love &lt;3</b> | <a href="/info/credits">Contributors!</a> | <b>Lucky Number: <?= $randomNumber ?></b>
+				<br><b style=" font-family: Arial,Helvetica,sans-serif !important">Made by grace with love &lt;3</b> | <a href="/info/credits" style=" font-family: Arial,Helvetica,sans-serif !important">Contributors!</a> | <b style=" font-family: Arial,Helvetica,sans-serif !important">Lucky Number: <?= $randomNumber ?></b>
 			</span>
 			<br style="display:block;clear:both;">
 		</p>
