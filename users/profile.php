2349f6ed92d98642d27d6c739dce5b6adc619542
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-03-01 23:38:13 +0000

    ah whatever v5

--- users/profile.php
+++ users/profile.php
@@ -70,7 +70,7 @@
 		<link rel="stylesheet" href="/css/new/comments.css?v=1">
 		<link rel="stylesheet" href="/css/new/stuff.css?v=1">
 		<link rel="stylesheet" href="/css/new/my/profile.css?v=7">
-		<link rel="stylesheet" href="/css/new/main.css?v=4">
+		<link rel="stylesheet" href="/css/new/main.css?v=5">
 		<link rel="stylesheet" href="/users/<?= $get_user->id ?>/css?t=<?= time() ?>">
 	
 	
