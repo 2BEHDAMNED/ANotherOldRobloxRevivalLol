58f6dc468dd721fec114e0802f7995371a6d12eb
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-03-03 17:04:13 +0000

    profile.css fixes

--- users/profile.php
+++ users/profile.php
@@ -69,7 +69,7 @@
 		<link rel="icon" type="image/x-icon" href="/favicon.ico">
 		<link rel="stylesheet" href="/css/new/comments.css?v=1">
 		<link rel="stylesheet" href="/css/new/stuff.css?v=1">
-		<link rel="stylesheet" href="/css/new/my/profile.css?v=7">
+		<link rel="stylesheet" href="/css/new/my/profile.css?v=8">
 		<link rel="stylesheet" href="/css/new/main.css?v=6">
 		<link rel="stylesheet" href="/users/<?= $get_user->id ?>/css?t=<?= time() ?>">
 	
